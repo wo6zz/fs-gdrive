b24b71d921edf78c3d9c41efb736df542fef954b
Ahmed <wo6zz@github.com>
2024-08-24 19:04:44 +0300

    feat: add ts setup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.mjs',
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript(), nodeResolve(), commonjs(), json()],
+  },
+];
